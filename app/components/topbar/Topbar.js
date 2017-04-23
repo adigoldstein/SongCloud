f642ab0191c12e6929fbdaf32790d53224daca02
Adi Goldstein <adij82@gmail.com>
2017-04-23 12:11:19 +0300

    Search bar finds by click

--- app/components/topbar/Topbar.js
+++ app/components/topbar/Topbar.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import {NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 import './topbar.scss';
 export default class Topbar extends React.Component {
   constructor() {
     super();
   }
+
+  searchClickHandler() {
+    if (this.textInput.value !== '') {
+      this.props.history.push(`/explore/${this.textInput.value}?search=true`);
+      this.textInput.value = '';
+    }
+  }
+
   inputChangeHandler(e) {
-    if (e.key === 'Enter' && this.textInput.value !== '' ) {
-      console.info(this.textInput.value);
+    if (e.key === 'Enter' && this.textInput.value !== '') {
+
 
       this.props.history.push(`/explore/${this.textInput.value}?search=true`);
+      this.textInput.value = '';
     }
-
   }
 
   render() {
@@ -30,14 +38,18 @@ export default class Topbar extends React.Component {
 
         <div className="rightnav">
           <div className="input-container">
-            <i className="search-icon fa fa-search" aria-hidden="true"></i>
-
+            <i className="search-icon fa fa-search" aria-hidden="true" onClick={() => {
+              this.searchClickHandler()
+            }}></i>
             <input className="search-input"
                    type="text"
                    placeholder="SEARCH"
                    ref={(input) => {
-                     this.textInput = input;}}
-                   onKeyUp={(e) => {this.inputChangeHandler(e)}}/>
+                     this.textInput = input;
+                   }}
+                   onKeyUp={(e) => {
+                     this.inputChangeHandler(e)
+                   }}/>
           </div>
           <button className="sign-out-btn"><a href="#">Logout</a></button>
         </div>
